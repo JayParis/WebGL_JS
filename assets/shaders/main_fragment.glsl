d443714e71dc76e47a9613c2319bda94d7031888
JayParis <jay.paris32@gmail.com>
2023-07-06 11:50:20 +0100

    Blur test

--- assets/shaders/main_fragment.glsl
+++ assets/shaders/main_fragment.glsl
@@ -5,5 +5,24 @@ uniform sampler2D sampler;
 
 void main()
 {
-   gl_FragColor = texture2D(sampler, fragTexCoord);
+   const float Pi = 6.28318530718;
+    
+   const float Directions = 16.0;
+   const float Quality = 4.0;
+
+   float Radius = 0.025;
+
+   vec4 Color = vec4(0);
+
+   for(float d = 0.0; d<Pi; d += Pi/Directions)
+   {
+      for(float i = 1.0 / Quality; i <= 1.001; i += 1.0 / Quality)
+      {
+         Color += texture2D(sampler, fragTexCoord + vec2(cos(d),sin(d))*Radius*i);		
+      }
+   }
+
+   Color /= Quality * Directions + 1.0;
+
+   gl_FragColor = vec4(Color.xyz, 1.0);
 }
